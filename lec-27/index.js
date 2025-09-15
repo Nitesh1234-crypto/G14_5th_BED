796d5acf5ac93e34f8343c6558913f3251c49ecb
Nitesh <nitesh.e21@nsut.ac.in>
2025-09-15 12:55:47 +0530

    frontend of blog app

--- lec-27/index.js
+++ lec-27/index.js
@@ -4,10 +4,11 @@ const app = express();
 const userRouter= require("./routes/userRoutes")
 const blogRouter= require("./routes/blogRoutes");
 const authRouter= require("./routes/authRoutes")
-const jwt=require("jsonwebtoken")
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-//middleware to verify jwt token
+app.use(express.static(__dirname+"/public"))
+
 app.use("/api/users",userRouter)
 app.use("/api/blogs",blogRouter)
 app.use("/api/auth",authRouter)
