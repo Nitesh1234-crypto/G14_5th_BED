d330630ac3437be1983b3230b3cae9a9ae0fb601
Nitesh <nitesh.e21@nsut.ac.in>
2025-09-15 12:56:12 +0530

    frontent of blog app continue

---
+++ lec-28/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+const userRouter= require("./routes/userRoutes")
+const blogRouter= require("./routes/blogRoutes");
+const authRouter= require("./routes/authRoutes")
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(__dirname+"/public"))
+
+app.use("/api/users",userRouter)
+app.use("/api/blogs",blogRouter)
+app.use("/api/auth",authRouter)
+
+mongoose.connect('mongodb://127.0.0.1:27017/G27DBs')
+  .then(() => console.log('Connected!'));
+app.listen(5556,()=>{
+    console.log("server started")
+})
---
+++ lec-28/public/index.js
@@ -0,0 +1,82 @@
+let signupForm= document.querySelector("#signup");
+let signupUserName= document.querySelector("#signup-username");
+let signupEmail= document.querySelector("#signup-email");
+let signupPassword= document.querySelector("#signup-password");
+
+signupForm.addEventListener("submit",async function(e){
+    e.preventDefault();
+    let usernameValue= signupUserName.value ;
+    let userEmailValue= signupEmail.value;
+    let userPasswordValue= signupPassword.value;
+     let response=await fetch("/api/users/",{
+                        method:"POST",
+                        body:JSON.stringify({
+                            username:usernameValue,
+                            email:userEmailValue,
+                            password:userPasswordValue
+                        }),
+                        headers:{
+                            "content-type":"application/json"
+                        }
+                    })
+    let data= await response.json();
+    console.log(data);
+    if(data.success){
+        alert("user added successfull, please login to continue");
+    }else{
+        alert("something went wrong")
+    }
+    
+
+
+})
+let loginForm= document.querySelector("#login");
+let loginEmail= document.querySelector("#login-email");
+let loginPassword= document.querySelector("#login-password");
+loginForm.addEventListener("submit",async function(e){
+    e.preventDefault();
+    let userEmailValue= loginEmail.value;
+    let userPasswordValue= loginPassword.value;
+     let response=await fetch("/api/auth/login",{
+                        method:"POST",
+                        body:JSON.stringify({
+                            email:userEmailValue,
+                            password:userPasswordValue
+                        }),
+                        headers:{
+                            "content-type":"application/json",
+                        }
+                    })
+    let data= await response.json();
+    console.log(data);
+    if(data.success){
+        alert("login successfull");
+        let token=data.token;
+        localStorage.setItem("token",token)
+    }else{
+        alert("something went wrong")
+    }
+    
+})
+
+
+let addBlogForm= document.querySelector("#addblog");
+let title= document.querySelector("#title");
+let body=document.querySelector("#body");
+
+addBlogForm.addEventListener("submit",async function(e){
+    e.preventDefault();
+    let titleValue= title.value ;
+    let bodyValue= body.value;
+   let response=await fetch("/api/blogs/",{
+        method:"POST",
+        body:JSON.stringify({title:titleValue,content:bodyValue}),
+        headers:{
+            "content-type":"application/json",
+            "authorization":localStorage.getItem("token"),
+        }
+    })
+    let data=await response.json();
+    console.log(data);
+
+})
