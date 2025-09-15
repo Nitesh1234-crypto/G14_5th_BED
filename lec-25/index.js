8164c357f430da683eceedc01f9e89d85e5f2901
Nitesh <nitesh.e21@nsut.ac.in>
2025-09-15 12:58:10 +0530

    authentication

--- lec-25/index.js
+++ lec-25/index.js
@@ -3,8 +3,52 @@ const app= express();
 const mongoose= require("mongoose");
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+const User= require("./model/user")
+
+app.post("/api/users/signup",async(req,res)=>{
+    let {username,email,password} = req.body;
+    let userExist=await User.findOne({email:email});
+    if(userExist){
+        return res.json({
+            success:false,
+            message:"User already exist with this email , please login to continue"
+        })
+    }
+    let newUser= await User.create({
+        username:username,
+        email:email,
+        password:password
+    })
+    res.json({
+        success:true,
+        message:"User sign up successful, please login to continue"
+    })
+
+})
+app.post("/api/auth/login",async(req,res)=>{
+    const {email,password} = req.body;
+    let userExist= await User.findOne({email:email});
+    if(!userExist){
+        return res.json({
+            success:false,
+            message:"Email not registered, please signup"
+        })
+    }
+    if(userExist.password!=password){
+        return res.json({
+            success:false,
+            message:"Incorrect password"
+        })
+    }
+    
+    res.json({
+        success:true,
+        message:"login successfull"
+    })
 
 
+
+})
 
 
 app.listen(5656,()=>{
